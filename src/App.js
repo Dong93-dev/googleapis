4a41a19ede1bac4d592fc8197a30bab1c3af4928
csocrates <calebsocrates@gmail.com>
2021-02-18 16:24:43 +0000

    creates camp search and nearby pubs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import GoogleMapReact from "google-map-react";
 import { Component } from "react";
-
+import { Link, Router } from "@reach/router";
+import PubFinder from "./PubFinder";
 class App extends Component {
   state = {
     defaultProps: {
       center: {
-        lat: 53.483959,
-        lng: -2.244644,
+        lat: 51.454514,
+        lng: -2.58791,
       },
       zoom: 11,
     },
   };
 
   componentDidMount() {
-    console.log("mount", this.state.places);
-    this.setState({ places: [] });
+    // console.log("mount", this.state.places);
+    this.setState({ places: [], place_id: "" });
   }
 
   handleApiLoaded = (map, maps) => {
@@ -30,50 +31,67 @@ class App extends Component {
       radius: "500",
       fields: ["name", "geometry"],
     };
+
     // use map and maps objects
-    console.dir(map);
-    console.dir(maps);
+    // console.dir(map);
+    // console.dir(maps);
     const service = new window.google.maps.places.PlacesService(map);
     console.dir(service);
     service.textSearch(request, (results, status) => {
       if (status == window.google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i++) {
+        for (let i = 0; i < 5; i++) {
           places.push(results[i]);
         }
-        console.log("places", results);
-        this.setState({ places });
+        const request2 = {
+          placeId: places[0].place_id,
+          // fields: ["website", "price_level", "rating"],
+        };
+        service.getDetails(request2, (results, status) => {
+          this.setState({ places, place_id: places[0].place_id, map });
+        });
       }
     });
   };
 
   render() {
     return (
-      <div className="App">
-        <div
-          id="map"
-          className="google-map"
-          style={{ height: "50vh", width: "100%" }}
-        >
-          {/* {this.state.map} */}
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: "AIzaSyAuc0iyyESvJUyPOLjHVn4j-RWcEBPrG0U",
-            }}
-            defaultCenter={this.state.defaultProps.center}
-            defaultZoom={this.state.defaultProps.zoom}
-            yesIWantToUseGoogleMapApiInternals
-            onGoogleApiLoaded={({ map, maps }) =>
-              this.handleApiLoaded(map, maps)
-            }
-          ></GoogleMapReact>
+      <>
+        <div className="App">
+          <div
+            id="map"
+            className="google-map"
+            style={{ height: "50vh", width: "100%" }}
+          >
+            {/* {this.state.map} */}
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: "AIzaSyAuc0iyyESvJUyPOLjHVn4j-RWcEBPrG0U",
+              }}
+              defaultCenter={this.state.defaultProps.center}
+              defaultZoom={this.state.defaultProps.zoom}
+              yesIWantToUseGoogleMapApiInternals
+              onGoogleApiLoaded={({ map, maps }) =>
+                this.handleApiLoaded(map, maps)
+              }
+            ></GoogleMapReact>
+          </div>
+          <h2>Camping near your location:</h2>
+          {this.state.places
+            ? this.state.places.map((place) => (
+                <p key={place.formatted_address}>
+                  {" "}
+                  <Link to={`/campsite/${place.place_id}`}>
+                    {" "}
+                    {place.formatted_address}
+                  </Link>{" "}
+                </p>
+              ))
+            : null}
         </div>
-
-        {this.state.places
-          ? this.state.places.map((place) => (
-              <p key={place.formatted_address}>{place.formatted_address} </p>
-            ))
-          : null}
-      </div>
+        <Router>
+          <PubFinder path="/campsite/:place_id" map={this.state.map} />
+        </Router>
+      </>
     );
   }
 }
