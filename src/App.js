6579478c3efad6b9126200dc04868fba36e3129a
Dong93-dev <dong.zhou93@outlook.com>
2021-02-18 09:42:51 +0000

    example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GoogleMapReact from "google-map-react";
 
 function App() {
+  let map;
+  let service;
+  let infowindow;
+  let places = [];
+  // map = new google.maps.Map();
+  const request = {
+    query: "campsites",
+    fields: ["name", "geometry"],
+  };
+  // service = new google.maps.places.PlacesService(map);
+  // service.textSearch(request, callback);
+  // function callback(results, status) {
+  //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+  //     for (var i = 0; i < results.length; i++) {
+  //       place.push(results[i]);
+  //     }
+  //   }
+  // }
+  const defaultProps = {
+    center: {
+      lat: 53.400002,
+      lng: -2.983333,
+    },
+    zoom: 11,
+  };
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="google-map" style={{ height: "50vh", width: "50%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAuc0iyyESvJUyPOLjHVn4j-RWcEBPrG0U" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
         >
-          Learn React
-        </a>
-      </header>
+          <AnyReactComponent lat={53.400002} lng={-2.983333} text="My Marker" />
+        </GoogleMapReact>
+      </div>
     </div>
   );
 }
